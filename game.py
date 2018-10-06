3a9f4de2a0531b97d992913d86ee6e0663e8e562
Malanius <malanius@seznam.cz>
2018-10-06 14:49:52 +0200

    Move the character

--- game.py
+++ game.py
@@ -23,6 +23,14 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+            elif event.key == pygame.K_LEFT:
+                x -= 1
+            elif event.key == pygame.K_RIGHT:
+                x += 1
+            elif event.key == pygame.K_UP:
+                y -= 1
+            elif event.key == pygame.K_DOWN:
+                y += 1
     
     # Prepare scene
     ##screen.fill((127,0,0))
